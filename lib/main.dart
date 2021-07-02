110b2857539f82fc81cc34b7884304c9de33ae6e
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-07-03 01:21:36 +0900

    flutter_riverpod install

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -87,11 +87,11 @@ class _AuthyState extends State<Authy> {
                         password: password,
                     );
 
-                    /*await Navigator.of(context).pushReplacement(
+                    await Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (context) {
-                          return DiaryList;
+                          return DiaryList();
                         }),
-                    );*/
+                    );
 
                   },
                 ),
@@ -111,11 +111,11 @@ class _AuthyState extends State<Authy> {
                           email: email,
                           password: password,
                       );
-                      /*await Navigator.of(context).pushReplacement(
+                      await Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (context) {
-                          return DiaryList;
+                          return DiaryList();
                         }),
-                      );*/
+                      );
 
                     },
               ),
@@ -142,5 +142,51 @@ class DiaryModel extends StatelessWidget {
     return Container();
   }
 }
+
+
+// TODO: class DiaryCreateの作成
+
+
+// TODO: class DiaryDetailの作成
+
+
+class DiaryList extends StatefulWidget {
+  /* 日記一覧 */
+   @override
+  _DiaryListState createState() => _DiaryListState();
+}
+
+class _DiaryListState extends State<DiaryList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('日記一覧'),
+        centerTitle: true,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        /*日記投稿画面への遷移ボタン*/
+        child: Icon(
+          Icons.create,
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => Authy(),
+          ));
+        },
+      ),
+
+
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8.0),
+
+      ),
+    );
+  }
+}
+
 
 
