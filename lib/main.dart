573a6a1771b668344b6c63b1833dd8c8175990dc
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-07-03 18:53:29 +0900

    日記一覧画面のモデル完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_analytics/observer.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -7,49 +6,39 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-
-
 
 final userProvider = StateProvider((ref) {
   /* Providerでユーザー情報の受け渡し有効化 */
   return FirebaseAuth.instance.currentUser;
 });
 
-
 final infoTextProvider = StateProvider.autoDispose((ref) {
   /* Providerでエラー情報の受け渡し有効化 */
   return '';
 });
-
 
 final emailProvider = StateProvider.autoDispose((ref) {
   /* Providerでメールアドレスの受け渡し有効化 */
   return '';
 });
 
-
 final passwordProvider = StateProvider.autoDispose((ref) {
   /* Providerでパスワードの受け渡し有効化 */
   return '';
 });
-
-
 
 final titleTextProvider = StateProvider.autoDispose((ref) {
   /* Providerで日記タイトルの受け渡し有効化 */
   return '';
 });
 
-
 final bodyTextProvider = StateProvider.autoDispose((ref) {
   /* Providerで日記本文の受け渡し有効化 */
   return '';
 });
-
-
-
 
 final postsQueryProvider = StreamProvider.autoDispose((ref) {
   /* 日記投稿時間の受け渡し有効化 */
@@ -59,21 +48,16 @@ final postsQueryProvider = StreamProvider.autoDispose((ref) {
       .snapshots();
 });
 
-
-
 // TODO:Providerで画像(URL?)の受け渡し有効化
-
-
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
-      ProviderScope( // Riverpodでのデータを受け渡し有効化
-        child: MyApp(),
-      ),
+    ProviderScope(
+      // Riverpodでのデータを受け渡し有効化
+      child: MyApp(),
+    ),
   );
 }
 
@@ -87,7 +71,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 
 class Authy extends StatefulWidget {
   /* 認証機能 */
@@ -106,14 +89,12 @@ class _AuthyState extends State<Authy> {
         title: Text('日記投稿アプリ'),
         centerTitle: true,
       ),
-
       body: Container(
         padding: EdgeInsets.all(24.0),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget> [
-
+            children: <Widget>[
               TextFormField(
                 /* メールアドレスの入力フォーム */
                 decoration: InputDecoration(labelText: 'メールアドレス'),
@@ -123,7 +104,6 @@ class _AuthyState extends State<Authy> {
                   });
                 },
               ),
-
               TextFormField(
                 /* パスワードの入力フォーム */
                 decoration: InputDecoration(labelText: 'パスワード'),
@@ -133,9 +113,7 @@ class _AuthyState extends State<Authy> {
                   });
                 },
               ),
-
               const SizedBox(height: 16.0),
-
               Container(
                 /* ユーザー登録ボタン */
                 width: double.infinity,
@@ -145,42 +123,38 @@ class _AuthyState extends State<Authy> {
                     final FirebaseAuth auth = FirebaseAuth.instance;
                     await auth.createUserWithEmailAndPassword(
                       /* emailとpasswordでユーザー登録する */
-                        email: email,
-                        password: password,
+                      email: email,
+                      password: password,
                     );
 
                     await Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) {
-                          return DiaryList();
-                        }),
+                      MaterialPageRoute(builder: (context) {
+                        return DiaryList();
+                      }),
                     );
-
                   },
                 ),
               ),
-
               const SizedBox(height: 8),
-
               Container(
                 /* ログインボタン */
                 width: double.infinity,
                 child: OutlinedButton(
                   child: Text('ログイン'),
-                    onPressed: () async {
-                      final FirebaseAuth auth = FirebaseAuth.instance;
-                      await auth.signInWithEmailAndPassword(
-                        /* emailとpasswordでユーザー認証する */
-                          email: email,
-                          password: password,
-                      );
-                      await Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) {
-                          return DiaryList();
-                        }),
-                      );
-
-                    },
-              ),
+                  onPressed: () async {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    await auth.signInWithEmailAndPassword(
+                      /* emailとpasswordでユーザー認証する */
+                      email: email,
+                      password: password,
+                    );
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) {
+                        return DiaryList();
+                      }),
+                    );
+                  },
+                ),
               )
             ],
           ),
@@ -189,7 +163,6 @@ class _AuthyState extends State<Authy> {
     );
   }
 }
-
 
 class DiaryModel extends StatelessWidget {
   /* 日記のモデル */
@@ -205,7 +178,6 @@ class DiaryModel extends StatelessWidget {
   }
 }
 
-
 // TODO: class DiaryCreateの作成
 class DiaryCreate extends ConsumerWidget {
   @override
@@ -219,7 +191,6 @@ class DiaryCreate extends ConsumerWidget {
         title: Text('日記作成'),
         centerTitle: true,
       ),
-
       body: SingleChildScrollView(
         padding: EdgeInsets.all(8.0),
         child: Column(
@@ -234,77 +205,50 @@ class DiaryCreate extends ConsumerWidget {
               maxLines: null,
               style: TextStyle(fontSize: 20.0),
 
-
               /* Providerから日記タイトルを更新 */
               onChanged: (String value) {
                 context.read(titleTextProvider).state = value;
               },
-
-
-
-
             ),
-
-
-            const SizedBox(height: 8.0,),
-
-
+            const SizedBox(
+              height: 8.0,
+            ),
             TextFormField(
               /* 本文フォーム */
               decoration: InputDecoration(
                 labelText: '内容',
                 border: OutlineInputBorder(),
               ),
-
               keyboardType: TextInputType.multiline,
               maxLines: null,
               style: TextStyle(fontSize: 20.0),
-
 
               /* Providerから本文を更新 */
               onChanged: (String value) {
                 context.read(bodyTextProvider).state = value;
               },
-
-
-
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-
-
                 FloatingActionButton(
-                  child: Icon(
-                    Icons.photo_library
-                  ),
+                  child: Icon(Icons.photo_library),
 
                   onPressed: null, // TODO:カメラロールにアクセスする
-
                 ),
-
-
-
-
-
                 ElevatedButton(
                   /* 投稿ボタン */
                   child: Text(
                     '投稿',
-                  style: TextStyle(fontSize: 15.0),
+                    style: TextStyle(fontSize: 15.0),
                   ),
-
-
-
                   onPressed: () async {
                     final postdate = DateTime.now().toLocal().toString();
                     final email = user.email;
                     final uid = user.uid;
 
-
                     await FirebaseFirestore.instance
-                    /* Firestoreへpostする日記データ */
+                        /* Firestoreへpostする日記データ */
                         .collection('post')
                         .doc()
                         .set({
@@ -313,90 +257,79 @@ class DiaryCreate extends ConsumerWidget {
                       'email': email,
                       'postdate': postdate,
                       'uid': uid,
-
                     });
                     Navigator.of(context).pop(); // 日記一覧画面へ戻る
-
-
-
-
-
-
-
-
-
                   },
                 )
-
-
-
-
-
               ],
             )
-
-
           ],
         ),
-
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 // TODO: class DiaryDetailの作成
 class DiaryDetail extends ConsumerWidget {
   /* 日記内容 */
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-
     final User user = watch(userProvider).state!;
     final AsyncValue<QuerySnapshot> asyncPostsQuery = watch(postsQueryProvider);
-
 
     return Scaffold(
       appBar: AppBar(
         title: Text('日記内容'),
         centerTitle: true,
       ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
 
 
+
+            Container(
+              child: Text('投稿日時'),
+            ),
+            /* 投稿日時 */
+
+            Container(
+              /* タイトル */
+              child: Text('タイトル'),
+            ),
+            Container(
+              /* 内容 */
+              child: Text('内容'),
+            ),
+            Container(
+              /* 画像 */
+              child: Text('画像'),
+            ),
+            ElevatedButton(
+              child: Text('一覧に戻る'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      ),
     );
-
-
-
   }
-
 }
-
 
 class DiaryList extends ConsumerWidget {
   /* 日記一覧 */
-   @override
-   Widget build(BuildContext context, ScopedReader watch) {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final User user = watch(userProvider).state!;
+    final AsyncValue<QuerySnapshot> asyncPostsQuery = watch(postsQueryProvider);
 
-     final User user = watch(userProvider).state!;
-     final AsyncValue<QuerySnapshot> asyncPostsQuery = watch(postsQueryProvider);
-
-
-
-     /* TODO:StreamBuilderを適用したい */
+    /* TODO:StreamBuilderを適用したい */
     return Scaffold(
       appBar: AppBar(
         title: Text('日記一覧'),
         centerTitle: true,
       ),
-
       floatingActionButton: FloatingActionButton(
         /*日記投稿画面への遷移ボタン*/
         child: Icon(
@@ -404,36 +337,26 @@ class DiaryList extends ConsumerWidget {
         ),
         onPressed: () {
           Navigator.push(
-            context,
-            MaterialPageRoute(
+              context,
+              MaterialPageRoute(
                 builder: (context) => DiaryCreate(), // DiaryCreateへ遷移
-          ));
+              ));
         },
       ),
-
-
       body: Column(
         children: [
-
           Expanded(
-
             child: asyncPostsQuery.when(
               /* 日記の読み込み状況による分岐 */
-
 
               data: (QuerySnapshot query) {
                 /* 日記の読み込みに成功した場合 */
                 return ListView(
                   children: query.docs.map((document) {
                     return Card(
-
-
                       /* 日記の要約 */
 
-
                       child: ListTile(
-
-
                         /* DiaryDetailへ遷移 */
                         title: TextButton(
                           onPressed: () {
@@ -446,67 +369,46 @@ class DiaryList extends ConsumerWidget {
                           child: Text(
                             document['titletext'],
                             style: TextStyle(fontSize: 30),
-                          ),),
-
+                          ),
+                        ),
 
                         leading: Text(document['postdate']),
                         subtitle: Text(document['uid']),
 
-
-
-
                         trailing: document['email'] == user.email
-                        /* メールアドレスでユーザーの確認を行いTrueならば削除ボタンを表示する */
+                            /* メールアドレスでユーザーの確認を行いTrueならば削除ボタンを表示する */
                             ? IconButton(
-                          icon: Icon(Icons.delete),
-                          /* 投稿日記削除ボタン */
-                          onPressed: () async {
-                            await FirebaseFirestore.instance
-                                .collection('post')
-                                .doc(document.id)
-                                .delete();
-                          },
-                        )
+                                icon: Icon(Icons.delete),
+                                /* 投稿日記削除ボタン */
+                                onPressed: () async {
+                                  await FirebaseFirestore.instance
+                                      .collection('post')
+                                      .doc(document.id)
+                                      .delete();
+                                },
+                              )
                             : null, // Falseの場合は表示しない
                       ),
-
-
                     );
                   }).toList(),
-
                 );
               },
-
               loading: () {
                 /* 日記読み込み中 */
                 return Center(
                   child: Text('読込中...'),
                 );
               },
-
               error: (e, stackTrace) {
                 /* 日記読み込み失敗した場合 */
                 return Center(
                   child: Text(e.toString()),
                 );
               },
-
             ),
-
-
           ),
-
-
-
-
         ],
       ),
-
-
-
     );
   }
 }
-
-
-
